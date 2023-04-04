5f342732cc7dda4b4d0d766b1cceffbf7d6f303e
andrecreppe <andrecrepper@gmail.com>
2023-04-04 17:30:37 -0300

    Semi-funcionando
    Ainda no caso do ex1.in ele está incluindo o valor de 0 novamente
    
    No runcodes performa 8/10

--- trabalho-01/t1.py
+++ trabalho-01/t1.py
@@ -1,23 +1,25 @@
+import os
+
 class Graph:
     def __init__(self, v):
         self.size = v
-        self.graph = [[] for x in range(v)]
-        self.graph_header = [0 for x in range(v)]
+        self.graph = [[] for _ in range(v)] # graph of lists
+        self.graph_header = [0 for _ in range(v)]
 
-    def addEdge(self, a, b):
-        self.graph[a].append(b)
-        self.graph_header[a] += 1
-        self.graph[b].append(a)
-        self.graph_header[b] += 1
+    def addEdge(self, v1, v2):
+        self.graph[v1].append(v2)
+        self.graph_header[v2] += 1
+        self.graph[v2].append(v1)
+        self.graph_header[v1] += 1
 
-    def printInfo(self):
-        print('Graph:')
+    def printGraph(self):
+        print('Graph (List Representation):')
         for i, v in enumerate(self.graph):
             count = self.graph_header[i]
             print(f'V[{i}] = {count} --> {v}')
 
     def isEuler(self):
-        # Every vertice should have an even number of connections
+        # Theorem: If every vertex has an *even* number of connections, it has an Euler Path 
         for v_count in self.graph_header:
             if (v_count % 2) != 0:
                 return False
@@ -25,36 +27,38 @@ class Graph:
         return True
     
     def findEuler(self):
-        visitedEdges = [[False for x in range(self.size)] for y in range(self.size)] # visit matrix
-
+        visitedEdges = [[False for _ in range(self.size)] for _ in range(self.size)] # Track visited lines
         startPos = 0
-        path = self.dfs(startPos, self.graph, visitedEdges, [])
+
+        path = self.dfs(startPos, self.graph, visitedEdges)
+
+        return path
+
+    def dfs(self, v0, graph, visitedEdges, path = []): # Depth-first search algorithm
+        path += [v0]
+        
+        for v1 in graph[v0]:
+            if visitedEdges[v0][v1] == False:
+                visitedEdges[v0][v1] = True
+                visitedEdges[v1][v0] = True
+
+                path = self.dfs(v1, graph, visitedEdges, path)
         
         return path
 
-    def dfs(self, start, graph, visitedEdges, path): # Depth-first search algorithm
-        path += [start]
-        
-        for v in graph[start]:
-            if visitedEdges[start][v] == False:
-                visitedEdges[start][v] = True
-                visitedEdges[v][start] = True
-                
-                path = self.dfs(v, graph, visitedEdges, path)
-        
-        return path
+# ================================================================= #
 
+## Read the file and Grpah Info ##
 
-# =================================================================
+here = os.path.dirname(os.path.abspath(__file__))
 
-## Read the file and Grpah Info
-
-# filename = input()
-filename = 'ex1.in'
-file = open(filename, 'r')
+filename = input()
+# filename = 'ex1.in'
+# filename = 'ex2.in'
+file = open(os.path.join(here, filename), 'r')
 v, a = file.readline().split(' ')
 
-## Populate the Graph with data
+## Populate the Graph with data ##
 
 graph = Graph(int(v))
 
@@ -66,8 +70,14 @@ for i in range(int(a)):
 
 file.close()
 
+# graph.printGraph()
+
+## Check and Solve for Euler Path ##
+
 if graph.isEuler():
+    path = graph.findEuler()
+    
     print('Sim')
-    print(graph.findEuler())
+    [print(n, end = ' ') for n in path]
 else:
     print('Não')
