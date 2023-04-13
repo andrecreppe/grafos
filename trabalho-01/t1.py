49d889a243ad53582404ed7b1979158b07a78416
andrecreppe <andrecrepper@gmail.com>
2023-04-12 21:01:36 -0300

    Update t1.py
    Working with Fleury's Algorithm

--- trabalho-01/t1.py
+++ trabalho-01/t1.py
@@ -8,15 +8,23 @@ class Graph:
 
     def addEdge(self, v1, v2):
         self.graph[v1].append(v2)
-        self.graph_header[v2] += 1
-        self.graph[v2].append(v1)
         self.graph_header[v1] += 1
+        self.graph[v2].append(v1)
+        self.graph_header[v2] += 1
 
-    def printGraph(self):
+    def removeEdge(self, v1, v2):
+        self.graph[v1].remove(v2)
+        self.graph_header[v1] -= 1
+        self.graph[v2].remove(v1)
+        self.graph_header[v2] -= 1
+
+    def printGraph(self): # for test pourposes
         print('Graph (List Representation):')
         for i, v in enumerate(self.graph):
             count = self.graph_header[i]
             print(f'V[{i}] = {count} --> {v}')
+
+    # ===============================================
 
     def isEuler(self):
         # Theorem: If every vertex has an *even* number of connections, it has an Euler Path 
@@ -26,24 +34,48 @@ class Graph:
             
         return True
     
-    def findEuler(self):
-        visitedEdges = [[False for _ in range(self.size)] for _ in range(self.size)] # Track visited lines
-        startPos = 0
+    # ===============================================
 
-        path = self.dfs(startPos, self.graph, visitedEdges)
-
-        return path
-
-    def dfs(self, v0, graph, visitedEdges, path = []): # Depth-first search algorithm
-        path += [v0]
+    def DFSCount(self, v, visited): # Deep Field Search to check bridges
+        count = 1
+        visited[v] = True
+		
+        for i in self.graph[v]:
+            if visited[i] == False:
+                count = count + self.DFSCount(i, visited)		
         
-        for v1 in graph[v0]:
-            if visitedEdges[v0][v1] == False:
-                visitedEdges[v0][v1] = True
-                visitedEdges[v1][v0] = True
+        return count
+    
+    def isNextEdgeValid(self, v0, v1):
+        # Case 01: v1 is the adjacent vertex of v0
+        if len(self.graph[v0]) == 1:
+            return True
+        else:
+            # Case 02: v0-v1 is not a bridge (it doesn't cut the graph)
+            visited = [False] * (self.size)
+            count1 = self.DFSCount(v0, visited)
 
-                path = self.dfs(v1, graph, visitedEdges, path)
-        
+            self.removeEdge(v0, v1)
+
+            visited = [False] * (self.size)
+            count2 = self.DFSCount(v0, visited)
+
+            self.addEdge(v0, v1)
+            self.graph[v0].sort()
+            self.graph[v1].sort()
+
+            comp = count1 > count2 # If count1 is greater, then edge v0-v1 is a bridge
+
+            return (not comp)
+    
+    def findEulerPath(self, v0, path = []):
+        for v1 in self.graph[v0]:
+            if self.isNextEdgeValid(v0, v1):
+                path += [v0]
+                self.removeEdge(v0, v1)
+
+                path = self.findEulerPath(v1, path)
+                
         return path
 
 # ================================================================= #
@@ -72,12 +104,16 @@ file.close()
 
 # graph.printGraph()
 
-## Check and Solve for Euler Path ##
+## Find the Euler Path if it exists ##
 
 if graph.isEuler():
-    path = graph.findEuler()
-    
+    startPos = 0
+    path = graph.findEulerPath(startPos)
+    path += [0] # end node
+
     print('Sim')
     [print(n, end = ' ') for n in path]
+    # print('\n0 1 2 3 1 4 3 0 << res') # ex1.in
+    # print('\n0 1 2 0 5 2 3 4 5 6 0 << res') # ex2.in
 else:
     print('Não')
